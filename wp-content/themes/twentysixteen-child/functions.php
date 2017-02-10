b7fcdccd9e4240c594866f99cb6a9e0a49376863
SlimTim10 <slimtim10@gmail.com>
2017-02-10 14:45:22 -0500

    Echo parent stylesheet for debugging

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -8,6 +8,7 @@ function my_theme_enqueue_styles() {
                     array( $parent_style ),
                     wp_get_theme()->get('Version')
   );
+  echo(get_stylesheet_uri());/* DEBUGGING */
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
