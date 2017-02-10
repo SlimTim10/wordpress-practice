178bbaceb98e17bd11daba1afadbac1215b2ef88
SlimTim10 <slimtim10@gmail.com>
2017-02-10 14:47:43 -0500

    Fix enqueue parent style

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -2,13 +2,12 @@
 
 function my_theme_enqueue_styles() {
   $parent_style = 'twentysixteen-style';
-  wp_enqueue_style( $parent_style, get_stylesheet_uri() );
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'child-style',
                     get_stylesheet_directory_uri() . '/style.css',
                     array( $parent_style ),
                     wp_get_theme()->get('Version')
   );
-  echo(get_stylesheet_uri());/* DEBUGGING */
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
